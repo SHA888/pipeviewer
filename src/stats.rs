6f80e0ebad5dac5cffb5fe1410f023fbfd7e9f18
Kresna Sucandra <kresnasucandra@gmail.com>
2022-09-23 07:32:22 +0800

    applying crossterm and colorizing the output

--- src/stats.rs
+++ src/stats.rs
@@ -1,11 +1,17 @@
 use crossbeam::channel::Receiver;
-use std::io::Result;
+use crossterm::{
+    cursor, execute,
+    style::{self, Color, PrintStyledContent, Stylize},
+    terminal::{Clear, ClearType},
+};
+use std::io::{self, Result, Stderr, Write};
 use std::time::{Duration, Instant};
 
 pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
     let start = Instant::now();
     let mut timer = Timer::new();
+    let mut stderr = io::stderr();
 
     loop {
         let num_bytes = stats_rx.recv().unwrap();
@@ -16,11 +22,11 @@ pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
 
         if !silent && timer.ready {
             timer.ready = false;
-            eprint!(
-                "\r{} {} [{:.0}b/s]",
+            output_progress(
+                &mut stderr,
                 total_bytes,
                 start.elapsed().as_secs().as_time(),
-                rate_per_second
+                rate_per_second,
             );
         }
         if num_bytes == 0 {
@@ -33,6 +39,21 @@ pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     Ok(())
 }
 
+fn output_progress(stderr: &mut Stderr, bytes: usize, elapsed: String, rate: f64) {
+    let bytes = style::style(format!("{} ", bytes)).with(Color::Red);
+    let elapsed = style::style(elapsed).with(Color::Green);
+    let rate = style::style(format!(" [{:.0}b/s]", rate)).with(Color::Blue);
+    let _ = execute!(
+        stderr,
+        cursor::MoveToColumn(0),
+        Clear(ClearType::CurrentLine),
+        PrintStyledContent(bytes),
+        PrintStyledContent(elapsed),
+        PrintStyledContent(rate),
+    );
+    let _ = stderr.flush();
+}
+
 trait TimeOutput {
     fn as_time(&self) -> String;
 }
@@ -43,7 +64,6 @@ impl TimeOutput for u64 {
         let (minutes, seconds) = (left / 60, left % 60);
         format!("{}:{:02}:{:02}", hours, minutes, seconds)
     }
-
 }
 
 struct Timer {
@@ -58,11 +78,11 @@ impl Timer {
     fn new() -> Self {
         let now = Instant::now();
         Self {
-            last_instant: now, 
-            delta: Duration::default(), 
-            period: Duration::from_millis(1000), 
-            countdown: Duration::default(), 
-            ready: true, 
+            last_instant: now,
+            delta: Duration::default(),
+            period: Duration::from_millis(1000),
+            countdown: Duration::default(),
+            ready: true,
         }
     }
     fn update(&mut self) {
@@ -73,6 +93,5 @@ impl Timer {
             self.ready = true;
             self.period
         });
-
     }
 }
